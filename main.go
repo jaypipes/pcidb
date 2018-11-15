a96e39f267c3a4df5a66ef1023c4ef335953b266
Jay Pipes <jaypipes@gmail.com>
2018-11-15 09:08:58 -0500

    remove PCI prefix and Id fields
    THIS IS A BACKWARDS-INCOMPATIBLE CHANGE
    
    Due to the fact that pcidb used to be a submodule in the ghw library,
    all of its publicly-exposed structs used to be prefixed with "PCI". Now
    that pcidb is its own library, there's no reason to continue using that
    prefix.
    
    Since this is a backwards-incompatible change, I took the opportunity to
    remove the old "Id" fields which had been deprecated in the 0.2 release.
    No use waiting around to remove those for a 1.0 release when this change
    is backwards incompatible anyway.
    
    Issue #9

--- main.go
+++ main.go
@@ -16,63 +16,51 @@ var (
 	cacheOnlyTrue = true
 )
 
-type PCIProgrammingInterface struct {
-	// Id is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent ID field.
-	Id   string
+// ProgrammingInterface is the PCI programming interface for a class of PCI
+// devices
+type ProgrammingInterface struct {
 	ID   string // hex-encoded PCI_ID of the programming interface
 	Name string // common string name for the programming interface
 }
 
-type PCISubclass struct {
-	// Id is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent ID field.
-	Id                    string
-	ID                    string                     // hex-encoded PCI_ID for the device subclass
-	Name                  string                     // common string name for the subclass
-	ProgrammingInterfaces []*PCIProgrammingInterface // any programming interfaces this subclass might have
+// Subclass is a subdivision of a PCI class
+type Subclass struct {
+	ID                    string                  // hex-encoded PCI_ID for the device subclass
+	Name                  string                  // common string name for the subclass
+	ProgrammingInterfaces []*ProgrammingInterface // any programming interfaces this subclass might have
 }
 
-type PCIClass struct {
-	// Id is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent ID field.
-	Id         string
-	ID         string         // hex-encoded PCI_ID for the device class
-	Name       string         // common string name for the class
-	Subclasses []*PCISubclass // any subclasses belonging to this class
+// Class is the PCI class
+type Class struct {
+	ID         string      // hex-encoded PCI_ID for the device class
+	Name       string      // common string name for the class
+	Subclasses []*Subclass // any subclasses belonging to this class
 }
 
+// Product provides information about a PCI device model
 // NOTE(jaypipes): In the hardware world, the PCI "device_id" is the identifier
 // for the product/model
-type PCIProduct struct {
-	// VendorId is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent VendorID field.
-	VendorId string
-	VendorID string // vendor ID for the product
-	// Id is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent ID field.
-	Id         string
-	ID         string        // hex-encoded PCI_ID for the product/model
-	Name       string        // common string name of the vendor
-	Subsystems []*PCIProduct // "subdevices" or "subsystems" for the product
+type Product struct {
+	VendorID   string     // vendor ID for the product
+	ID         string     // hex-encoded PCI_ID for the product/model
+	Name       string     // common string name of the vendor
+	Subsystems []*Product // "subdevices" or "subsystems" for the product
 }
 
-type PCIVendor struct {
-	// Id is DEPRECATED in 0.2 and will be removed in the 1.0 release. Please
-	// use the equivalent ID field.
-	Id       string
-	ID       string        // hex-encoded PCI_ID for the vendor
-	Name     string        // common string name of the vendor
-	Products []*PCIProduct // all top-level devices for the vendor
+// Vendor provides information about a device vendor
+type Vendor struct {
+	ID       string     // hex-encoded PCI_ID for the vendor
+	Name     string     // common string name of the vendor
+	Products []*Product // all top-level devices for the vendor
 }
 
 type PCIDB struct {
-	// hash of class ID -> class dbrmation
-	Classes map[string]*PCIClass
-	// hash of vendor ID -> vendor dbrmation
-	Vendors map[string]*PCIVendor
-	// hash of vendor ID + product/device ID -> product dbrmation
-	Products map[string]*PCIProduct
+	// hash of class ID -> class information
+	Classes map[string]*Class
+	// hash of vendor ID -> vendor information
+	Vendors map[string]*Vendor
+	// hash of vendor ID + product/device ID -> product information
+	Products map[string]*Product
 }
 
 // WithOption is used to represent optionally-configured settings
